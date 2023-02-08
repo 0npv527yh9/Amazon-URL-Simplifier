6214834657d95d8d1521ba3e8293182fcf70ab1c
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2023-02-09 03:14:02 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Amazon-URL-Simplifier
 Bookmarklet to simplify Amazon product page URL
+
+## How to install
+- Install the contents of [`bookmarklet.txt`](bookmarklet.txt) as a bookmarklet in your browser.
+
+## How to use
+1. Access a Amazon product page.
+1. Click this bookmarklet.
+
+## What can it do
+1. Simplify Amazon product page URL.
+1. Write the simplified URL in the address bar.
+1. Copy the URL to the clipboard.
---
+++ amazon_url-simplifier.js
@@ -0,0 +1,53 @@
+(()=> {
+const amazonHost = 'www.amazon.co.jp';
+
+
+function main() {
+    const url = location.href;
+
+    if (isAmazonProductPage(url)) {
+        const newUrl = simplifyUrl(url);
+
+        // Write newUrl in the address bar
+        window.history.pushState({}, '', newUrl);
+
+        // Copy newUrl to the clipboard
+        copy(newUrl);
+    } else {
+        alert("This is not a amazon product page.");
+    }
+}
+main();
+
+
+// string -> boolean
+function isAmazonProductPage(url) {
+    const isAmazonPage = new URL(url).host == amazonHost;
+    const isProductPage = url.includes('/dp/');
+    return isAmazonPage && isProductPage;
+}
+
+
+// string -> string
+function simplifyUrl(url) {
+    const dp = '/dp/';
+    const asinLength = 10;
+
+    const asinBegin = url.indexOf(dp) + dp.length;
+    const asinEnd = asinBegin + asinLength;
+    const asin = url.substring(asinBegin, asinEnd);
+
+    const newUrl = location.origin + dp + asin;
+    return newUrl;
+}
+
+
+// string -> void
+function copy(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Copied: " + text);
+    }, () => {
+        alert("Failed");
+    });
+}
+})()
