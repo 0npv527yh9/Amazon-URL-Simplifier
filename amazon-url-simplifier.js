d22645c1bfc6e821c45fb260c458dce054edf5c8
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2023-02-09 18:19:35 +0900

    format

--- amazon-url-simplifier.js
+++ amazon-url-simplifier.js
@@ -1,53 +1,52 @@
-(()=> {
-const amazonHost = 'www.amazon.co.jp';
+(() => {
 
+    function main() {
+        const url = location.href;
 
-function main() {
-    const url = location.href;
+        if (isAmazonProductPage(url)) {
+            const newUrl = simplifyUrl(url);
 
-    if (isAmazonProductPage(url)) {
-        const newUrl = simplifyUrl(url);
+            // Write newUrl in the address bar
+            window.history.pushState({}, '', newUrl);
 
-        // Write newUrl in the address bar
-        window.history.pushState({}, '', newUrl);
-
-        // Copy newUrl to the clipboard
-        copy(newUrl);
-    } else {
-        alert("This is not a amazon product page.");
+            // Copy newUrl to the clipboard
+            copy(newUrl);
+        } else {
+            alert("This is not a amazon product page.");
+        }
     }
-}
-main();
+    main();
 
 
-// string -> boolean
-function isAmazonProductPage(url) {
-    const isAmazonPage = new URL(url).host == amazonHost;
-    const isProductPage = url.includes('/dp/');
-    return isAmazonPage && isProductPage;
-}
+    // string -> boolean
+    function isAmazonProductPage(url) {
+        const amazonHost = 'www.amazon.co.jp';
+        const isAmazonPage = new URL(url).host == amazonHost;
+        const isProductPage = url.includes('/dp/');
+        return isAmazonPage && isProductPage;
+    }
 
 
-// string -> string
-function simplifyUrl(url) {
-    const dp = '/dp/';
-    const asinLength = 10;
+    // string -> string
+    function simplifyUrl(url) {
+        const dp = '/dp/';
+        const asinLength = 10;
 
-    const asinBegin = url.indexOf(dp) + dp.length;
-    const asinEnd = asinBegin + asinLength;
-    const asin = url.substring(asinBegin, asinEnd);
+        const asinBegin = url.indexOf(dp) + dp.length;
+        const asinEnd = asinBegin + asinLength;
+        const asin = url.substring(asinBegin, asinEnd);
 
-    const newUrl = location.origin + dp + asin;
-    return newUrl;
-}
+        const newUrl = location.origin + dp + asin;
+        return newUrl;
+    }
 
 
-// string -> void
-function copy(text) {
-    navigator.clipboard.writeText(text).then(() => {
-        alert("Copied: " + text);
-    }, () => {
-        alert("Failed");
-    });
-}
+    // string -> void
+    function copy(text) {
+        navigator.clipboard.writeText(text).then(() => {
+            alert("Copied: " + text);
+        }, () => {
+            alert("Failed");
+        });
+    }
 })()
